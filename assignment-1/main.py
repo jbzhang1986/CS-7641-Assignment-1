1acd048ab07e1fb18e329b5a2c846e83e0bccd64
Adrian Chang <amchang87@gmail.com>
2017-09-11 23:37:37 -0700

    cleanup cli

---
+++ assignment-1/main.py
@@ -0,0 +1,17 @@
+'''main.py
+
+The driver to the entire assignment
+'''
+import argparse
+
+parser = argparse.ArgumentParser(prog='main.py')
+subparsers = parser.add_subparsers(title='subcommands', dest='command')
+
+stats_parser = subparsers.add_parser('stats', help='show some stats about the data')
+knn_parser = subparsers.add_parser('knn', help='run k-nearest neighbors')
+knn_parser.add_argument('-n', '--number', type=int, help='Amount of neighbors')
+args = parser.parse_args()
+
+# print something out!
+if not args.command:
+    parser.print_help()
