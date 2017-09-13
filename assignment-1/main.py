0fcd44481df9e2c161761da768999ebf1e54dfe8
Adrian Chang <amchang87@gmail.com>
2017-09-12 23:23:45 -0700

    import data sets

--- assignment-1/main.py
+++ assignment-1/main.py
@@ -8,6 +8,10 @@ parser = argparse.ArgumentParser(prog='main.py')
 subparsers = parser.add_subparsers(title='subcommands', dest='command')
 
 stats_parser = subparsers.add_parser('stats', help='show some stats about the data')
+
+cleaner_parser = subparsers.add_parser('clean', help='clean the stats from original to final')
+
+
 knn_parser = subparsers.add_parser('knn', help='run k-nearest neighbors')
 knn_parser.add_argument('-n', '--number', type=int, help='Amount of neighbors')
 args = parser.parse_args()
@@ -17,5 +21,6 @@ if not args.command:
     parser.print_help()
 
 command = args.command
-if command == 'stats':
-  pass
+if command == 'cleaner':
+elif command == 'stats':
+    pass
