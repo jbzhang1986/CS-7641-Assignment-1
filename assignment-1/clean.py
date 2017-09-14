6498bbee14745c5de749dce20730160a0f4768af
Adrian Chang <amchang87@gmail.com>
2017-09-13 23:41:09 -0700

    finish wine cleaning

--- assignment-1/clean.py
+++ assignment-1/clean.py
@@ -3,3 +3,26 @@
 Make the datasets correct for the experiment
 '''
 import pandas as pd
+import logging 
+logger = logging.getLogger(__name__)
+
+def create_final_datasets():
+    ''' Munge the datasets
+    '''
+    logger.info('Loading datasets')
+    create_wine_dataset()
+    
+def create_wine_dataset():
+    wine_red = pd.read_csv('./data/winequality-red-original.csv', sep=';')
+    wine_red['red'] = 1
+    wine_white = pd.read_csv('./data/winequality-white-original.csv', sep=';')
+    wine_white['red'] = 0
+    logger.info('Red wine dataframe shape %s', wine_red.shape)
+    logger.info('White wine dataframe shape %s', wine_white.shape)
+    logger.info('Randomly selecting wines to match red')
+    wine_white_final = wine_white.sample(wine_red.shape[0], random_state=0)
+    wine_final = wine_red.append(wine_white_final)
+    logger.info('Final wine set information \n %s', wine_final.describe())
+
+def create_credit_card_dataset():
+    pass
--- assignment-1/main.py
+++ assignment-1/main.py
@@ -3,24 +3,26 @@
 The driver to the entire assignment
 '''
 import argparse
+import clean
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser(prog='main.py')
-subparsers = parser.add_subparsers(title='subcommands', dest='command')
+if __name__ == '__main__':
+    # parse here
+    parser = argparse.ArgumentParser(prog='main.py')
+    subparsers = parser.add_subparsers(title='subcommands', dest='command')
+    cleaner_parser = subparsers.add_parser('clean', help='Clean the stats from original to final and show me information')
+    knn_parser = subparsers.add_parser('knn', help='run k-nearest neighbors')
+    knn_parser.add_argument('-n', '--number', type=int, help='Amount of neighbors')
+    args = parser.parse_args()
 
-stats_parser = subparsers.add_parser('stats', help='show some stats about the data')
+    # print something out!
+    if not args.command:
+        parser.print_help()
 
-cleaner_parser = subparsers.add_parser('clean', help='clean the stats from original to final')
-
-
-knn_parser = subparsers.add_parser('knn', help='run k-nearest neighbors')
-knn_parser.add_argument('-n', '--number', type=int, help='Amount of neighbors')
-args = parser.parse_args()
-
-# print something out!
-if not args.command:
-    parser.print_help()
-
-command = args.command
-if command == 'cleaner':
-elif command == 'stats':
-    pass
+    command = args.command
+    if command == 'clean':
+        clean.create_final_datasets()
+    elif command == 'knn':
+        pass
